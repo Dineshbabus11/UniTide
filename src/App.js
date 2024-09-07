3ad7b1af9484e52fa65c4223a7674cd9cbbe3211
Dineshbabus11 <msgtodineshbabus@gmail.com>
2024-09-07 13:04:02 +0530

    Donation portal was route successfully

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/Basic/Footer/Footer';
 import { Header } from './components/Basic/Header/Header';
 import { Update } from './components/Basic/Update/Update';
 import { Home } from './components/pages/home/Home';
-
+import { Donate } from './components/pages/Donate/Donate';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Header/>
       <div className='main-content'>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/donate' element={<Donate/>}/>
+        </Routes>
         <Update/>
       </div>
       <Footer/>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/pages/Donate/Donate.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Donate.css'
+
+export const Donate = () => {
+  return (
+    <div className='donate-portal'>Donate</div>
+  )
+}
